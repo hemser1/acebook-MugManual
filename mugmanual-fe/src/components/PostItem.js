677108c6ecf861dcecd7d17da29056732b651b33
Student <student@Makerss-MacBook-Pro.local>
2019-08-23 12:03:43 +0100

    date formate changed to time ago

--- mugmanual-fe/src/components/PostItem.js
+++ mugmanual-fe/src/components/PostItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactTimeAgo from 'react-time-ago'
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Card from 'react-bootstrap/Card';
@@ -7,12 +8,13 @@ import PropTypes from 'prop-types';
 export class PostItem extends Component {
   render() {
     const { id, username, message, created_at, user_id } = this.props.post;
+    var date1 = new Date(created_at);
     return (
       <div>
         <center><Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>{username}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">{created_at}</Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">Posted: <ReactTimeAgo date={date1}/></Card.Subtitle>
           <Card.Text>
             {message}
           </Card.Text>
