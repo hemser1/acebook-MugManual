4e4e439d9e2fa1fb9d0f66c15d6d61a65c3ddece
ted_scanlan <t.scanlan92@gmail.com>
2019-09-17 12:53:39 +0100

    changed time formatting

--- mugmanual-fe/src/index.js
+++ mugmanual-fe/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import JavascriptTimeAgo from 'javascript-time-ago';
+
+import en from 'javascript-time-ago/locale/en';
+import ru from 'javascript-time-ago/locale/ru';
+
+JavascriptTimeAgo.locale(en)
+JavascriptTimeAgo.locale(ru)
 
 ReactDOM.render(<App />, document.getElementById('root'));
